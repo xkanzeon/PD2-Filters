66d4f5ae23f7ed867e0a3762cd707411088f96da
Kanzeon <72973313+xkanzeon@users.noreply.github.com>
2024-03-25 00:48:25 -0500

    Implement Mewtiny's master list

--- downloadFilters.py
+++ downloadFilters.py
@@ -1,29 +1,46 @@
-import requests
-import json
+# Rebased onto Mewtiny's master list creation
+# Mostly just added the batch download for this repo
+
+import urllib.request as req, json
 from pathlib import Path
 
-authorListJson = json.loads(requests.get('https://raw.githubusercontent.com/Project-Diablo-2/LootFilters/main/filters.json').text)
-for author in authorListJson:
-    authorFolder = Path(__file__).parent / 'filters' / author['author']
-    Path(authorFolder).mkdir(parents=True, exist_ok=True)
+pd2FilterListUrl = "https://raw.githubusercontent.com/Project-Diablo-2/LootFilters/main/filters.json"
+masterFilterList = {}
 
-    authorContentsJson = requests.get(author['url']).json()
-    if 'message' in authorContentsJson:
-        print(authorContentsJson['message'])
-        break
-    for file in authorContentsJson:
-        try:
-            if file['name'].endswith('.filter'):
-                filterName = file['name'].replace('.filter', '')
-                print(f"Downloading {author['author']}/{filterName}...")
-                Path(authorFolder / filterName).mkdir(parents=True, exist_ok=True)
-                with open(Path(authorFolder) / filterName / 'loot.filter', 'wb') as filter:
-                    filterResponse = requests.get(file['download_url'])
-                    filterResponse.encoding = 'cp1252'
-                    if 'message' in filterResponse:
-                        print(filterResponse['message'])
-                        break
-                    filter.write(filterResponse.content)
-        except Exception as e:
-          print(f"Error writing file [{file['name']}]: {e}")
-          continue
+with req.urlopen(pd2FilterListUrl) as url:
+    authorsData = json.load(url)
+
+if authorsData:
+    for d in authorsData:
+        authorName  = d["author"]
+        displayName = d["name"]
+        authorUrl   = d["url"]
+
+        masterFilterList[authorName] = {}
+        
+        masterFilterList[authorName]["info"] = d
+        masterFilterList[authorName]["filters"] = []
+
+        with req.urlopen(authorUrl) as url:
+            filterData = json.load(url)
+        
+        if filterData:
+            for f in filterData:
+                if f['name'].endswith('.filter'):
+                    masterFilterList[authorName]["filters"].append({
+                        "name": f["name"],
+                        "url": f["url"],
+                        "download_url": f["download_url"]
+                    })
+
+                    with req.urlopen(f["download_url"]) as url:
+                        url.headers.set_param("charset", "cp1252")
+                        filterText = url.read()
+                    
+                    Path(Path(__file__).parent / 'filters' / authorName / f['name']).mkdir(parents=True, exist_ok=True)
+                    with open(Path(__file__).parent / 'filters' / authorName / f['name'] / 'loot.filter', 'wb') as filter:
+                        filter.write(filterText)
+
+    jsonData = json.dumps(sorted(masterFilterList.items()), indent=4)
+    with open("pd2_filters.json", "w") as j:
+        j.write(jsonData)
